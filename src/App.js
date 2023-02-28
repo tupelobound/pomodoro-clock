1ad541e64fc0df7a0b445d0d0703985f73ccf142
tupelobound <29886944+tupelobound@users.noreply.github.com>
2023-02-28 20:28:44 +0000

    Set initial states and added break controller

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css'
+import Break from './Break'
+import { useState } from 'react'
 
 function App() {
+  // Set initial state
+  const [state, setState]= useState({
+    breakLength: 5,
+    sessionLength: 25,
+    timerTotalSeconds: 25 * 60,
+    timerMinutes: 25,
+    timerSeconds: "00",
+    running: "initialized",
+    color: "white",
+    timerMode: "Session"
+  })
+
+  const handleClick = () => {
+    console.log('Testing')
+  }
+  
   return (
-    <div>
-      <p>
-        Hello World!
-      </p>
+    <div id="main">
+      <h1>Pomodoro Clock</h1>
+      <Break length={state.breakLength} click={handleClick} />
     </div>
   )
 }
