5ead5f77c633613d7fef3e0a26523029f1e5398f
tupelobound <29886944+tupelobound@users.noreply.github.com>
2023-03-04 22:32:23 +0000

    Fixed ESLint errors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
+function App () {
   return (
     <div>
       <p>Hello World!</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
