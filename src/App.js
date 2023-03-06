db2f7b40949993c59c8a2f46f6da9f194291907d
tupelobound <29886944+tupelobound@users.noreply.github.com>
2023-03-06 21:58:50 +0000

    Incorporated timer and cancel button in app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
 import './App.css'
 import { React, useState } from 'react'
 import LengthSetter from './LengthSetter'
+import Timer from './Timer'
+import CancelButton from './CancelButton'
 
 function App () {
   // set initial states
   const [breakLength, setBreakLength] = useState(5)
   const [sessionLength, setSessionLength] = useState(25)
+  const [timerMinutes, setTimerMinutes] = useState(25)
+  const [timerSeconds, setTimerSeconds] = useState('00')
+  const [color, setColor] = useState('white')
+  const [timerMode, setTimerMode] = useState('Session')
 
   return (
     <div id='main'>
       <h1>Pomodoro Clock</h1>
-      <LengthSetter name="break" length={breakLength} setLength ={setBreakLength} />
-      <LengthSetter name="session" length={sessionLength} setLength={setSessionLength} />
+      <LengthSetter
+        name="break"
+        length={breakLength}
+        setLength={setBreakLength}
+      />
+      <LengthSetter
+        name="session"
+        length={sessionLength}
+        setLength={setSessionLength}
+      />
+      <Timer
+        timerMinutes={timerMinutes}
+        timerSeconds={timerSeconds}
+        color={color}
+        timerMode={timerMode}
+      />
+      <CancelButton
+        setBreakLength={setBreakLength}
+        setSessionLength={setSessionLength}
+        setTimerMinutes={setTimerMinutes}
+        setTimerSeconds={setTimerSeconds}
+        setColor={setColor}
+        setTimerMode={setTimerMode}
+      />
     </div>
   )
 }
