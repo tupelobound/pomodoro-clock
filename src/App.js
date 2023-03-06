a2e8ffc5dd9ca9c7cb7b8e68167ec5517fad4baa
tupelobound <29886944+tupelobound@users.noreply.github.com>
2023-03-06 07:36:49 +0000

    Added LengthSetter components to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react'
 import './App.css'
+import { React, useState } from 'react'
+import LengthSetter from './LengthSetter'
 
 function App () {
+  // set initial states
+  const [breakLength, setBreakLength] = useState(5)
+  const [sessionLength, setSessionLength] = useState(25)
+
   return (
-    <div>
-      <p>Hello World!</p>
+    <div id='main'>
+      <h1>Pomodoro Clock</h1>
+      <LengthSetter name="Break" length={breakLength} setLength ={setBreakLength} />
+      <LengthSetter name="Session" length={sessionLength} setLength={setSessionLength} />
     </div>
   )
 }
