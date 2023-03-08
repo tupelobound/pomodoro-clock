ed8cac5c4ad554dfc783ced618e1cfffb6d887cf
tupelobound <29886944+tupelobound@users.noreply.github.com>
2023-03-08 21:58:12 +0000

    Added timer functionality

--- src/PlayButton.js
+++ src/PlayButton.js
@@ -1,16 +1,41 @@
-import React from 'react'
+import { React, useEffect } from 'react'
+import PropTypes from 'prop-types'
 
 // Functional component for the play/pause button
-const PlayButton = () => {
+const PlayButton = ({ status, setStatus, icon, setIcon, timerMinutes, setTimerMinutes }) => {
   const playPause = (e) => {
-    console.log(e)
+    if (status !== 'running') {
+      setStatus('running')
+      setIcon('fas fa-pause fa-3x')
+    } else {
+      setStatus('paused')
+      setIcon('fas fa-play fa-3x')
+    }
   }
+
+  useEffect(() => {
+    if (status === 'running') {
+      const interval = setInterval(() => {
+        setTimerMinutes(timerMinutes => timerMinutes - 1)
+      }, 1000)
+      return () => clearInterval(interval)
+    }
+  }, [status])
 
   return (
     <>
-      <i id="start_stop" className="fas fa-play fa-3x" onClick={playPause} />
+      <i id="start_stop" className={icon} onClick={playPause} />
     </>
   )
+}
+
+PlayButton.propTypes = {
+  status: PropTypes.string,
+  setStatus: PropTypes.func,
+  icon: PropTypes.string,
+  setIcon: PropTypes.func,
+  timerMinutes: PropTypes.number,
+  setTimerMinutes: PropTypes.func
 }
 
 export default PlayButton
