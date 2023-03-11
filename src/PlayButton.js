4ef2fd440420b71668d598e3ee26b685ecda4402
tupelobound <29886944+tupelobound@users.noreply.github.com>
2023-03-11 00:15:19 +0000

    Transferred playPause function into PlayButton component

--- src/PlayButton.js
+++ src/PlayButton.js
@@ -3,7 +3,20 @@ import { DataContext } from './context/DataContext'
 
 // Functional component for the play/pause button
 const PlayButton = () => {
-  const { icon, playPause } = useContext(DataContext)
+  const { icon, status, setStatus, setIcon } = useContext(DataContext)
+
+  // Declare function for changing status and play/pause button icon when pressed
+  const playPause = (e) => {
+    // Check status of app, if not 'running':
+    if (status !== 'running') {
+      setStatus('running') // set status to 'running'
+      setIcon('fas fa-pause fa-3x') // change icon to 'pause'
+    } else {
+      setStatus('paused') // set status to 'paused'
+      setIcon('fas fa-play fa-3x') // change icon to 'play'
+    }
+  }
+
   // return JSX component
   return (
     <>
